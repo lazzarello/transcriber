a6088d9d3e64db43dd3a314344b874bda406cd92
Lee Azzarello <lee@rockingtiger.com>
2025-01-31 13:37:51 -0800

    update PyTorch 2.6.1

--- engine/run.sh
+++ engine/run.sh
@@ -2,12 +2,9 @@
 docker run -it --user lee --rm \
   --gpus all \
   --runtime=nvidia \
-  -e NVIDIA_VISIBLE_DEVICES=all \
-  -e NVIDIA_DRIVER_CAPABILITIES=all \
   -v $(pwd):/app \
   -v /run/user/1000/pipewire-0:/tmp/pipewire-0 \
   -e XDG_RUNTIME_DIR=/tmp \
   -v $HOME/.cache/huggingface/hub:$HOME/.cache/huggingface/hub \
   -v $HOME/.cache/huggingface/datasets:$HOME/.cache/huggingface/datasets \
-  transcriber-engine:pipewire-whisper \
-  /bin/bash
+  transcriber-engine:cpu-only \
